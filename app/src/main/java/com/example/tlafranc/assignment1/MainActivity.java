c0d500dc4ebc4970a45467bd24602317f80f2243
tlafranc <33941124+tlafranc@users.noreply.github.com>
2018-01-29 20:01:43 -0700

    List view is displayable but not updatable (bug)

--- app/src/main/java/com/example/tlafranc/assignment1/MainActivity.java
+++ app/src/main/java/com/example/tlafranc/assignment1/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,13 +17,39 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Subscription> subscriptionList;
     private ArrayAdapter<Subscription> adapter;
-
+    private double totalCharge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sublist = (ListView) findViewById(R.id.main_sub_list);
+
+        /*
+        sublist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //this
+            }
+        });
+        */
+
     }
+
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        subscriptionList = new ArrayList<Subscription>();
+        Subscription testSub = new Subscription("Netflix", "1997-10-06", "10", "Comment");
+        subscriptionList.add(testSub);
+        adapter = new ArrayAdapter<Subscription>(this, R.layout.list_item, subscriptionList);
+        sublist.setAdapter(adapter);
+        setTotalCharge();
+    }
+
 
     /** Called when the user taps the "Add a Subscription" button */
     public void onAddSubscriptionClick (View view) {
@@ -38,11 +62,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         String [] subInfo = data.getStringArrayExtra("SubInformation");
         Subscription newSub = new Subscription(subInfo[0], subInfo[1], subInfo[2], subInfo[3]);
         subscriptionList.add(newSub);
-
+        adapter.notifyDataSetChanged();
+        System.out.println(subscriptionList);
+        //setTotalCharge();
 
     }
+
+    private void setTotalCharge() {
+        totalCharge = 0.00;
+        for (int i = 0; i < subscriptionList.size(); i++) {
+            totalCharge += Double.parseDouble(subscriptionList.get(i).getCharge());
+        }
+        System.out.println("Total Charge is " + Double.toString(totalCharge));
+        TextView text = (TextView) findViewById(R.id.TotalCharge);
+        text.setText("Total Monthly Charge: $" + Double.toString(totalCharge));
+    }
+
 }
--- app/src/main/java/com/example/tlafranc/assignment1/Subscription.java
+++ app/src/main/java/com/example/tlafranc/assignment1/Subscription.java
@@ -1,13 +1,59 @@
 package com.example.tlafranc.assignment1;
 
 import java.util.Date;
+import java.util.jar.Attributes;
 
 /**
  * Created by Thomas on 2018-01-26.
  */
 
 class Subscription {
+    private String name;
+    private String date;
+    private String charge;
+    private String comment;
+
     public Subscription(String name, String date, String charge, String comment) {
-        super();
+        this.name = name;
+        this.date = date;
+        this.charge = charge;
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCharge() {
+        return charge;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setCharge(String charge) {
+        this.charge = charge;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString(){
+        return this.name + "\nDate: " + this.date + "    Charge: $" + this.charge;
     }
 }
